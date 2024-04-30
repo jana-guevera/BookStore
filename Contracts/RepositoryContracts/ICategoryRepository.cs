b2ab25c28697f2b93d823f522725915fe2fa9c91
Jana <jeevananthanjana@gmail.com>
2024-04-30 18:23:31 +0530

    Created IRepository and ICategoryRepository

---
+++ Contracts/RepositoryContracts/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts.RepositoryContracts
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+
+    }
+}
---
+++ Contracts/RepositoryContracts/IRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.RepositoryContracts
+{
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// Find and returns single record based on the provided expression
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        Task<T> Get(Expression<Func<T, bool>> filter);
+
+        /// <summary>
+        /// Returns all the records from the table
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<T>> GetAll();
+
+        /// <summary>
+        /// Add the record to the table
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        Task<T> Add(T item);
+
+        /// <summary>
+        /// Update the record from the table
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        Task<T> Update(T item);
+    }
+}
