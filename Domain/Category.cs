b7f48d8af7c4e1f0cf18760c56ca1f39317fe1c4
Jana <jeevananthanjana@gmail.com>
2024-04-30 17:36:52 +0530

    Created the category domain model

---
+++ Domain/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="The category name is required")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The category display order is required")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
