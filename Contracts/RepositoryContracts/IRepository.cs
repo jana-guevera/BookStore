faddcfbde46a76c405be2ce4717d13920db1bc24
Jana <jeevananthanjana@gmail.com>
2024-05-01 14:50:33 +0530

    Renamed IRepository Methods and added Remove and FindMany methods

--- Contracts/RepositoryContracts/IRepository.cs
+++ Contracts/RepositoryContracts/IRepository.cs
@@ -9,30 +9,44 @@ namespace Contracts.RepositoryContracts
     public interface IRepository<T> where T : class
     {
         /// <summary>
-        /// Find and returns single record based on the provided expression
-        /// </summary>
-        /// <param name="filter"></param>
-        /// <returns></returns>
-        Task<T> Get(Expression<Func<T, bool>> filter);
-
-        /// <summary>
-        /// Returns all the records from the table
-        /// </summary>
-        /// <returns></returns>
-        Task<IEnumerable<T>> GetAll();
-
-        /// <summary>
-        /// Add the record to the table
+        /// Add the record to the table and returns the record
         /// </summary>
         /// <param name="item"></param>
         /// <returns></returns>
         Task<T> Add(T item);
 
         /// <summary>
-        /// Update the record from the table
+        /// Update the record from the table and returns the record
         /// </summary>
         /// <param name="item"></param>
         /// <returns></returns>
         Task<T> Update(T item);
+
+        /// <summary>
+        /// Removes a record from the table and returns the record
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<T> Remove(int id);
+
+        /// <summary>
+        /// Find and returns single record based on the provided expression
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        Task<T> FindOne(Expression<Func<T, bool>> filter);
+
+        /// <summary>
+        /// Find and returns multiple records based on the provided expression
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> FindMany(Expression<Func<T, bool>> filter);
+
+        /// <summary>
+        /// Returns all the records from the table
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<T>> GetAll();
     }
 }
