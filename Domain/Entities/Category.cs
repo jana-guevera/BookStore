bc8450fc16e424af9d6a0cc1c4f2338e88fe77ac
Jana <jeevananthanjana@gmail.com>
2024-05-01 15:31:36 +0530

    Modified the Category model

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -11,12 +11,13 @@ namespace Domain.Entities
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "The category name is required")]
+        [Required(ErrorMessage = "{0} is required")]
         [MaxLength(100)]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "The category display order is required")]
+        [Required(ErrorMessage = "{0} is required")]
         [DisplayName("Display Order")]
+        [Range(0, int.MaxValue, ErrorMessage = "{0} should be between {1} and {2}")]
         public int DisplayOrder { get; set; }
     }
 }
