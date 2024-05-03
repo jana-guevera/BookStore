c05a881f119acc9ebef63304e8634a6fbfdd6aae
Jana <jeevananthanjana@gmail.com>
2024-05-03 14:20:18 +0530

    Added entity framework core and created BookStoreDbContext

---
+++ Repositories/Database/BookStoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories.Database
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
