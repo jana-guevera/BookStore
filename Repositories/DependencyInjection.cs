78bdfd0fa3ace4ac1ee1b8c95f2bd188bd0d5736
Jana <jeevananthanjana@gmail.com>
2024-05-03 18:14:40 +0530

    Created Category Repository and added it to DI

--- Repositories/DependencyInjection.cs
+++ Repositories/DependencyInjection.cs
@@ -1,18 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Contracts.RepositoryContracts;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Repositories.Database;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Repositories.Repos;
 
 namespace Repositories
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddRepositoryServices(this IServiceCollection services, IConfiguration configuration) {
+            // DbContext Service
             services.AddDbContext<BookStoreDbContext>(options => 
                                    options.UseSqlServer(configuration.GetConnectionString("default")));
+
+            // Repository Service
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
 
             return services;
         }
---
+++ Repositories/Repos/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.RepositoryContracts;
+using Domain.Entities;
+using Repositories.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories.Repos
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BookStoreDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
