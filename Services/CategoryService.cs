0db15e63f810f26ca0a67360200b75a9466d6d20
Jana <jeevananthanjana@gmail.com>
2024-05-03 18:03:21 +0530

    Implimented the categoryService

---
+++ Services/CategoryService.cs
@@ -0,0 +1,46 @@
+﻿using Contracts.RepositoryContracts;
+using Contracts.ServicesContracts;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<Category> AddAsync(Category category)
+        {
+            return await _categoryRepository.AddAsync(category);
+        }
+
+        public async Task<Category> RemoveAsync(int id)
+        {
+            Category category = new Category() { Id = id };
+            return await _categoryRepository.RemoveAsync(category);
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            return await _categoryRepository.UpdateAsync(category);
+        }
+
+        public async Task<Category> FindByIdAsync(int id)
+        {
+            return await _categoryRepository.FindOneAsync(x => x.Id == id);
+        }   
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _categoryRepository.GetAllAsync();
+        }
+    }
+}
