0570a20c8a8982a4621a7e1fc9f5b9f91701533c
Jana <jeevananthanjana@gmail.com>
2024-05-05 18:43:57 +0530

    Implimented the add method in CategoryService

--- Services/CategoryService.cs
+++ Services/CategoryService.cs
@@ -1,10 +1,10 @@
-﻿using Contracts.RepositoryContracts;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Contracts.RepositoryContracts;
 using Contracts.ServicesContracts;
 using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+using Domain.ErrorHandling;
+using Services.Helpers;
 
 namespace Services
 {
@@ -19,6 +19,20 @@ namespace Services
 
         public async Task<Category> AddAsync(Category category)
         {
+            // Check if category is null and throw error
+            if (category == null) throw new NoArgumentError("Empty Category");
+
+            // Validate the category data and throw error 
+            ValidationHelper.ModelValidation(category);
+
+            // Check if there is an existing category with the same name and throw error
+            Category existingCategory = await _categoryRepository.FindByName(category.Name);
+            if (existingCategory != null)
+            {
+                throw new UniqueValidationError("Category Already Exists");
+            }
+            
+            // create category and return the category
             return await _categoryRepository.AddAsync(category);
         }
 
