6fcbce2f35121d387a8f9f561f475422924f980e
Jana <jeevananthanjana@gmail.com>
2024-05-05 18:41:02 +0530

    Update category domain model

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -12,12 +12,12 @@ namespace Domain.Entities
         public int Id { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
-        [MaxLength(100)]
+        [MaxLength(20)]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
         [DisplayName("Display Order")]
-        [Range(1, 1000, ErrorMessage = "{0} should be between {1} and {2}")]
+        [Range(1, 100, ErrorMessage = "{0} should be between {1} and {2}")]
         public int DisplayOrder { get; set; }
     }
 }
