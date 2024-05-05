e6c75480e81f7957fef0870d55b5564fd6a11a18
Jana <jeevananthanjana@gmail.com>
2024-05-05 18:43:32 +0530

    Created a model validation helper class

---
+++ Services/Helpers/ValidationHelper.cs
@@ -0,0 +1,29 @@
+﻿using Domain.ErrorHandling;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Services.Helpers
+{
+    public class ValidationHelper
+    {
+        /// <summary>
+        /// Validates the model and throws ValidationError if validation failed
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <exception cref="ValidationError"></exception>
+        public static void ModelValidation(object obj)
+        {
+            ValidationContext validationContext = new ValidationContext(obj);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+            
+            if (!isValid)
+            {
+                throw new ValidationError("Model validation failed", validationResults);
+            }
+        }
+    }
+}
