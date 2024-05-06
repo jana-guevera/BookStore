10dff144ffecc293d77e2477e738080fd6328653
Jana <jeevananthanjana@gmail.com>
2024-05-06 19:53:13 +0530

    Created user interface for category page

---
+++ BookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Contracts.ServicesContracts;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService; 
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Category> categories = await _categoryService.GetAllAsync();
+            return View(categories);
+        }
+    }
+}
