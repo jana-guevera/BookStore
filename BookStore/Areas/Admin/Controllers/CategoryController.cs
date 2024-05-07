79d2e9e02eabe3f7f4c2501fc8f094986c3fc7cb
Jana <jeevananthanjana@gmail.com>
2024-05-07 19:50:22 +0530

    Created the interface and controller method for category crud operations

--- BookStore/Areas/Admin/Controllers/CategoryController.cs
+++ BookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -21,5 +21,81 @@ namespace BookStore.Areas.Admin.Controllers
             IEnumerable<Category> categories = await _categoryService.GetAllAsync();
             return View(categories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category) {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            await _categoryService.AddAsync(category);
+            TempData["success"] = "Category added successfully";
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Category category = await _categoryService.FindByIdAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            Category updatedCategory = await _categoryService.UpdateAsync(category);
+
+            if(updatedCategory == null) { return NotFound(); }
+			TempData["success"] = "Category updated successfully";
+			return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Remove(int Id)
+        {
+            await _categoryService.RemoveAsync(Id);
+			TempData["success"] = "Category removed successfully";
+			return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Handles remote validation for category name field
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [AcceptVerbs("Get", "Post")]
+        public async Task<IActionResult> IsCategoryExist(string name, int? Id)
+        {
+            Category category = await _categoryService.FindByName(name);
+
+            if (category == null) return Json(true);
+
+            if (Id == null)
+            {
+                return Json($"Category name is already in use");
+            }else if ((int)Id == category.Id)
+            {
+                return Json(true);
+            }
+
+
+            return Json($"Category name is already in use");
+        }
     }
 }
